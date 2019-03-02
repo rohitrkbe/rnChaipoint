b32b5b727be52486b788cba865fbbe0e6f985795
Rohit <rohitkumar@Rohits-MacBook-Pro.local>
2019-03-02 22:12:35 +0530

    initial commit

--- source/index.js
+++ source/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, AsyncStorage } from "react-native";
+import { View } from "react-native";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 import InitialScreen  from "./screen";
@@ -11,7 +11,6 @@ export class App extends Component {
     return (
       <Provider store={store}>
         <View>
-          <Text>hi</Text>
           <InitialScreen />
         </View>
       </Provider>
--- source/screen/index.js
+++ source/screen/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { View, Text, AsyncStorage, } from "react-native";
+import { View, Text, ScrollView,StyleSheet } from "react-native";
 import { connect } from 'react-redux';
 import { getApiResponse } from "../api";
+import VideoPlayer from "./Video";
 
 class InitialScreen extends Component {
   constructor(props) {
@@ -10,65 +11,116 @@ class InitialScreen extends Component {
     dataLoaded: false,
     };
   }
-  
   componentDidMount() {
     console.log('componentDidMount')
     this.fetchData();
     // BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
   };
-  
   fetchData(){
     this.props.fetchApiResponse();
   }
-
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     this.setState({ dataLoaded: false });
     if (nextProps && nextProps.getAPIResponse !== this.props.getAPIResponse) {
       this.setState({ dataLoaded: true });
     }
   }
-
   componentWillUnmount() {
     // BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
   }
-
   handleBackPress = async () => {
     Actions.pop();
     return true
   }
-
   render() {
-    console.log('response in render',this.props.getAPIResponse);
+    const section=[];
+    for (let i=0;i<10;i++){
+      section.push(<Section sectionNumber={i+1}/>);
+    }
     return (
-      <View>
+      <ScrollView>
         <Text>initial Screen</Text>
         {
           this.state.dataLoaded==true
           ?
             this.props.getAPIResponse!=='error'
             ?
-              <View><Text>Data Loaded</Text></View> 
+              <View><Text>getAPIResponse Data Loaded</Text></View> 
             :
               <View><Text>Error in loading data</Text></View>
           :
-            <View><Text>Data is loading...</Text></View>
+            <View><Text>getAPIResponse Data is loading...</Text></View>
         }
-      </View>
+        {section}
+      </ScrollView>
     );
   }
 }
-
 const mapStateToProps = (state) => {
   const { 
-    getAPIResponse
+    getAPIResponse,
   } = state.SampleReducer;
   return {
     getAPIResponse
   };
 };
-
 const mapDispatchToProps = dispatch => ({
   fetchApiResponse: () => dispatch(getApiResponse()),
 });
-
 export default connect(mapStateToProps,mapDispatchToProps)(InitialScreen);
+
+class Section extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      duration:null,
+      currentTime:null,
+    };
+  }
+  totalDuration(duration){
+    this.setState({duration});
+  }
+  durationPlayed(currentTime){
+    this.setState({currentTime});
+  }
+  render() {
+    const videos=[];
+    for (let i=0;i<10;i++){
+      videos.push(
+        <VideoPlayer/>
+      );
+    }
+    console.log(videos);
+    return (
+      <View style={styles.viewConatainer}>
+        <Text style={{color:'black',fontWeight:'bold'}}>Section number - {this.props.sectionNumber}</Text>
+        <VideoPlayer videoPlayed={(currentTime)=>this.durationPlayed(currentTime)} videoDuration={(duration)=>this.totalDuration(duration)}/>
+        <View style={styles.timeShowView}>
+          <Text style={styles.textColor}>Total Duration Time : {this.state.duration}</Text>
+          <Text style={styles.textColor}>Current Time : {this.state.currentTime}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  viewConatainer: {
+    height:400,
+    backgroundColor : 'white',
+    marginVertical: 5,
+    marginHorizontal: 5,
+    elevation:3,
+    padding:5,
+  },
+  videoConatainer:{
+  },
+  timeShowView:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+  },
+  textColor:{
+    color:'black',
+  }
+});
